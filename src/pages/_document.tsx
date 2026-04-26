a6f5764220811ff60130738870358cd696c3f761
Bamdad Nabavianpour <157402490+BWMMGW@users.noreply.github.com>
2026-04-26 10:08:28 +0330

    Create _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="alternate icon" href="/favicon.ico" />
+      <Head />
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+        
+      </body>
+    </Html>
+  );
+}
